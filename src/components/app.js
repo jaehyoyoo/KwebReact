5ab65a954e527f9e6d51d7cdff5d694cf9fad8c9
yoojaehyo <dbwogy1@naver.com>
2017-06-10 15:41:59 +0900

    initial commit

--- src/components/app.js
+++ src/components/app.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Content from './content.js';
 
-var App = React.createClass ({
-    render: function () {
-        return (
-        <div>
-            Main Page
-        </div>
-        );
-    }
-});
+class App extends React.Component {
+	constructor(props){
+		super(props);
+		this.state = {
+
+		};
+		// 함수선언 : this.함수이름 = this.함수.bind(this) 하고 아래에서 내용 선언
+	}
+	render(){
+		return (
+			<div> Hello world! </div>
+		);
+	}
+}
 
 export default App;
